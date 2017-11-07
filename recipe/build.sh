98d620d9d10cd782f660e94b6634782d97eaabff
Zhe Wang <0x1998@gmail.com>
2017-11-07 21:01:04 +0800

    Fix libgrpc++.so versioning

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e -x
 
+sed -ri 's|(libgrpc\+\+[^\]*?so)\.5$|\1.1|g' Makefile
 export CFLAGS='-Wno-error -Wno-error=expansion-to-defined'
 make $MAKEFLAGS prefix=$PREFIX "EXTRA_CFLAGS=$FLAGS" "EXTRA_CXXFLAGS=$CFLAGS"
 make $MAKEFLAGS prefix=$PREFIX install
