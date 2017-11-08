ab4c1238cc32fc4e03a3b0b487a4ace7938c1720
Zhe Wang <0x1998@gmail.com>
2017-11-08 10:18:13 +0800

    Fix dependencies

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,5 +3,11 @@ set -e -x
 
 sed -ri 's|(libgrpc\+\+[^\]*?so)\.5$|\1.1|g' Makefile
 export CFLAGS='-Wno-error -Wno-error=expansion-to-defined'
-make $MAKEFLAGS prefix=$PREFIX "EXTRA_CFLAGS=$FLAGS" "EXTRA_CXXFLAGS=$CFLAGS"
+make $MAKEFLAGS prefix=$PREFIX \
+    HAS_SYSTEM_PERFTOOLS=false \
+    HAS_SYSTEM_ZLIB=true \
+    HAS_SYSTEM_PROTOBUF=true \
+    HAS_SYSTEM_CARES=true \
+    "EXTRA_CFLAGS=$FLAGS" \
+    "EXTRA_CXXFLAGS=$CFLAGS"
 make $MAKEFLAGS prefix=$PREFIX install
